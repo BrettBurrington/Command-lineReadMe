6b918c70e06399ab60a4a7b29587a8a2649c3cdd
brett burrington <bburrington13@gmail.com>
2023-07-23 17:02:13 -0700

    the rest of the propmts

--- index.js
+++ index.js
@@ -8,10 +8,51 @@ function promptUser() {
     {
       type: 'input',
       name: 'projectTitle',
-      message: 'Enter the title of your amazing project:',
+      message: 'Enter the title of your project:',
     },
-   
-
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Describe your project:',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Provide instruction for how to install',
+    },
+     {
+      type: 'input',
+      name: 'usage',
+      message: 'Provide dome of the usage information:',
+    },
+    {
+      type: 'input',
+      name: 'contribution',
+      message: 'Provide some guidelines for contibution to the project:',
+    },
+     {
+      type: 'input',
+      name: 'tests',
+      message: 'State the test instructions:',
+    },
+     {
+      type: 'list',
+      name: 'license',
+      message: 'Select a license that applies:',
+      choices: ['MIT', 'Apache', 'GNU', 'None'],
+    },
+   {
+      type: 'input',
+      name: 'githubUsername',
+      message: 'Enter your GitHub username:',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Provide an email address for inquiries:',
+    },
   ]);
+
+ 
 }
 
